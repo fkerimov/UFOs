f47361db358ad18864db341c802bcf38eaf76810
Farhad Kerimov <farhad.kerimov@gmail.com>
2021-03-10 13:40:29 -0500

    Adding app.js with functions

--- static/js/app.js
+++ static/js/app.js
@@ -5,13 +5,24 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 function buildTable(data) {
-    tbody.html("");         // Clear the table so that there is no pre-filtered data on consequtive searches
-    data.forEach((dataRow) => {     // Next, loop through each object in the data and append a row and cells for each value in the row
-        let row = tbody.append("tr");   // Append a row to the table body
+    tbody.html("");                              // Clear the table so that there is no pre-filtered data on consequtive searches
+    data.forEach((dataRow) => {                 // Next, loop through each object in the data and append a row and cells for each value in the row
+        let row = tbody.append("tr");           // Append a row to the table body
     });
     
     Object.values(dataRow).forEach((val) => {   // Loop through each field in the dataRow and add each value as a table cell (td)
         let cell = row.append("td");
         cell.text(val);
     });
-}
+};
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");                    // Grab the datetime value from the filter
+    let filteredData = tableData;
+    
+    if (date) {                                                            // Check to see if a date was entered and filter the data using that date.
+        filteredData = filteredData.filter(row => row.datetime === date); // Apply 'filter' to the table data to only keep the rows where the 'datetime' value matches the filter value
+    };
+    buildTable(filteredData);                    // Rebuild the table using the filtered data // @NOTE: If not date was entered, then filteredData will just be original tableData 
+};
+
